6b54dbf7a730627d3cbc3be2d907b2697bc60f69
panzifan <2092907971@qq.com>
2020-05-18 22:05:50 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,24 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    background: ['../../res/test1_kb.jpg', '../../res/test2_kb.jpg'],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: false,
+    interval: 3000,
+    duration: 1000
+},
+// btnToDo:function(){
+//   wx.showActionSheet({
+//     itemList: ['A', 'B', 'C'],
+//     success (res) {
+//       console.log(res.tapIndex)
+//     },
+//     fail (res) {
+//       console.log(res.errMsg)
+//     }
+//   })  
+// }
+ })
---
+++ pages/partiton/partiton.js
@@ -0,0 +1,56 @@
+Page({
+  data:{
+    text:"Page partition"
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  },
+  basketball_handler:function(){
+   console.log(1)
+  },
+  football_handler:function(){
+    console.log(2)
+   },
+   volleyball_handler:function(){
+    console.log(3)
+   },
+   pingpong_handler:function(){
+    console.log(4)
+   },
+  computer_handler:function(){
+    console.log(5)
+   },
+   fit_handler:function(){
+    console.log(6)
+   },
+   badminton_handler:function(){
+    console.log(7)
+   },
+   yoka_handler:function(){
+    console.log(8)
+   },
+   running_handler:function(){
+    console.log(9)
+   },
+   swimming_handler:function(){
+    console.log(10)
+   },
+   equipment_handler:function(){
+    console.log(11)
+   },
+   else_handler:function(){
+    console.log(12)
+   },
+})
