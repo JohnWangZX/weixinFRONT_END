7f0a6a7e66dce05f04042f2046ed6e8f0e7ceff6
panzifan <2092907971@qq.com>
2020-05-22 18:00:08 +0800

    更新

--- pages/collect/collect.js
+++ pages/collect/collect.js
@@ -5,6 +5,36 @@ Page({
    * 页面的初始数据
    */
   data: {
+    hotList: [
+      {
+        coverImg: 'http://i2.hdslb.com/bfs/archive/1239539a2f262d933bca7c2c1e290139420ba76a.jpg_320x200.jpg',
+        title: '【乐正绫】《华夏之章》【小旭PRO】【绛舞乱丸】',
+        playNum: '4.7万',
+        commentNum: '977',
+        avid: 'av1'
+      },
+      {
+        coverImg: 'http://i1.hdslb.com/bfs/archive/ecce95b426faf188e6c28f9d3a0bdc63c5a72bb3.jpg_320x200.jpg',
+        title: '【斗图歌】装逼不如斗图',
+        playNum: '4.7万',
+        commentNum: '977',
+        avid: 'av2'
+      },
+      {
+        coverImg: 'http://i0.hdslb.com/bfs/archive/11bf8d41fffcad31976317760e301e2db64be8c8.png_320x200.png',
+        title: '【胖胖球】【双子星】【獒龙】荒岛 - El transcurrir de las horas',
+        playNum: '4.7万',
+        commentNum: '977',
+        avid: 'av3'
+      },
+      {
+        coverImg: 'http://i0.hdslb.com/bfs/archive/e73a92b0ed615b4d6568888906d09f84d0835674.jpg_320x200.jpg',
+        title: '撩人净土系列【红菱歌舞伎初音】极乐净土【大神犬PV付】MME配布',
+        playNum: '4.7万',
+        commentNum: '977',
+        avid: 'av4'
+      }
+    ]
 
   },
 
@@ -62,5 +92,6 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+ 
 })
